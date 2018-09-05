fe80b27e2703fc5aa31ca8aacd321de9ea221d14
Roberto Marcelo Trujillo <romarcetrujillo@outlook.com>
2018-09-04 22:31:13 -0300

    Inicializacón del Proyecto TamboCloud

---
+++ TamboCloud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TamboCloud.MvcApplication" Language="C#" %>
---
+++ TamboCloud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TamboCloud.Startup))]
+namespace TamboCloud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
